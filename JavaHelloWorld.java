6ab7cd29dfdafa9ae2b7ab01a19ba8e6ac4b1a92
yhansen <yannic.hansen@inform-software.com>
2016-10-07 10:29:42 +0200

    Delete JavaHelloWorld.java

--- JavaHelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class JavaHelloWorld
-{
-        public static void main (String [] args)
-                {
-                System.out.println("First Hello");
-                System.out.println("Second one");
-                }
-}
